1715f4fa0c43435c405673005e981c3ff0d0bd49
PowerShopMCPE <37243611+PowerShop@users.noreply.github.com>
2022-03-08 23:42:58 +0700

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,102 +1,145 @@
-const mineflayer = require('mineflayer')
-const Movements = require('mineflayer-pathfinder').Movements
-const pathfinder = require('mineflayer-pathfinder').pathfinder
-const { GoalBlock} = require('mineflayer-pathfinder').goals
+const mineflayer = require("mineflayer");
+const Movements = require("mineflayer-pathfinder").Movements;
+const pathfinder = require("mineflayer-pathfinder").pathfinder;
+const { GoalBlock } = require("mineflayer-pathfinder").goals;
 
-const config = require('./settings.json');
+const config = require("./settings.json");
 
-function createBot () {
-  const bot = mineflayer.createBot({
-      username: config['bot-account']['username'],
-      password: config['bot-account']['password'],
-      auth: config['bot-account']['type'],
-      host: config.server.ip,
-      port: config.server.port,
-      version: config.server.version
-  })
-
-  bot.loadPlugin(pathfinder)
-  const mcData = require('minecraft-data')(bot.version)
-  const defaultMove = new Movements(bot, mcData)
-  bot.settings.colorsEnabled = false
-
-  bot.once("spawn", function(){
-      console.log("\x1b[33m[BotLog] Bot joined to the server", '\x1b[0m')
-
-      if(config.utils['auto-auth'].enabled){
-        console.log("[INFO] Started auto-auth module")
-
-          var password = config.utils['auto-auth'].password
-          setTimeout(function() {
-              bot.chat(`/register ${password} ${password}`)
-              bot.chat(`/login ${password}`)
-          }, 500);
-
-          console.log(`[Auth] Authentification commands executed.`)
-      }
-
-      if(config.utils['chat-messages'].enabled){
-        console.log("[INFO] Started chat-messages module")
-        var messages = config.utils['chat-messages']['messages']
-
-          if(config.utils['chat-messages'].repeat){
-            var delay = config.utils['chat-messages']['repeat-delay']
-            let i = 0
-
-            let msg_timer = setInterval(() => {
-                bot.chat(`${messages[i]}`)
-
-                if(i+1 == messages.length){
-                    i = 0
-                } else i++
-            }, delay * 1000)
-          } else {
-              messages.forEach(function(msg){
-                  bot.chat(msg)
-              })
-        }
-      }
-      
-
-      const pos = config.position
-
-      if (config.position.enabled){
-          console.log(`\x1b[32m[BotLog] Starting moving to target location (${pos.x}, ${pos.y}, ${pos.z})\x1b[0m`)
-          bot.pathfinder.setMovements(defaultMove)
-          bot.pathfinder.setGoal(new GoalBlock(pos.x, pos.y, pos.z))
-      }
-      
-      if(config.utils['anti-afk'].enabled){
-        bot.setControlState('jump', true)
-        if(config.utils['anti-afk'].sneak){
-            bot.setControlState('sneak', true)
-        }
-      }
-  })
-
-  bot.on("chat", function(username, message){
-      if(config.utils['chat-log']){
-          console.log(`[ChatLog] <${username}> ${message}`)
-      }
-  })
-
-  bot.on("goal_reached", function(){
-      console.log(`\x1b[32m[BotLog] Bot arrived to target location. ${bot.entity.position}\x1b[0m`)
-  })
-
-  bot.on("death", function(){
-      console.log(`\x1b[33m[BotLog] Bot has been died and was respawned ${bot.entity.position}`, '\x1b[0m')
-  })
-
-  if(config.utils['auto-reconnect']){
-      bot.on('end', function(){
-        createBot()
-      })
+function countDown(number) {
+  console.log("กำลังจะเชื่อมต่อเซิร์ฟเวอร์ในอีก " + number + " วินาที");
+  if (number > 1) {
+      setTimeout(function(){
+        countDown(number - 10);
+      }, 10000)
+  } else {
+    console.log("\nเชื่อมต่อเซิร์ฟเวอร์แล้ว\n");
   }
-
-  bot.on('kicked', (reason) => console.log('\x1b[33m',`[BotLog] Bot was kicked from the server. Reason: \n${reason}`, '\x1b[0m'))
-  bot.on('error', err => console.log(`\x1b[31m[ERROR] ${err.message}`, '\x1b[0m'))
-
 }
 
-createBot()
+function createBot() {
+  const bot = mineflayer.createBot({
+    username: config["bot-account"]["username"],
+    password: config["bot-account"]["password"],
+    auth: config["bot-account"]["type"],
+    host: config.server.ip,
+    port: config.server.port,
+    version: config.server.version,
+  });
+
+  bot.loadPlugin(pathfinder);
+  const mcData = require("minecraft-data")(bot.version);
+  const defaultMove = new Movements(bot, mcData);
+  bot.settings.colorsEnabled = false;
+
+  bot.once("spawn", function () {
+    var name = config["bot-account"]["username"];
+    console.log("\x1b[33m[บันทึก] " + name + " ได้เข้าสู่เซิร์ฟเวอร์แล้ว", "\x1b[0m");
+
+    if (config.utils["auto-auth"].enabled) {
+      console.log("[แจ้งเตือน] เปิดระบบล็อคอินอัตโนมัติแล้ว");
+
+      var password = config.utils["auto-auth"].password;
+      setTimeout(function () {
+        bot.chat(`/register ${password} ${password}`);
+        bot.chat(`/login ${password}`);
+      }, 500);
+
+      console.log(`[ระบบล็อกอิน] ดำเนินการล็อกอินแล้ว`);
+    }
+
+    if (config.utils["chat-messages"].enabled) {
+      console.log("[INFO] Started chat-messages module");
+      var messages = config.utils["chat-messages"]["messages"];
+
+      if (config.utils["chat-messages"].repeat) {
+        var delay = config.utils["chat-messages"]["repeat-delay"];
+        let i = 0;
+
+        let msg_timer = setInterval(() => {
+          bot.chat(`${messages[i]}`);
+
+          if (i + 1 == messages.length) {
+            i = 0;
+          } else i++;
+        }, delay * 1000);
+      } else {
+        messages.forEach(function (msg) {
+          bot.chat(msg);
+        });
+      }
+    }
+
+    const pos = config.position;
+
+    if (config.position.enabled) {
+      console.log(
+        `\x1b[32m[บันทึก] Starting moving to target location (${pos.x}, ${pos.y}, ${pos.z})\x1b[0m`
+      );
+      bot.pathfinder.setMovements(defaultMove);
+      bot.pathfinder.setGoal(new GoalBlock(pos.x, pos.y, pos.z));
+    }
+
+    if (config.utils["anti-afk"].enabled) {
+      bot.setControlState("jump", true);
+      if (config.utils["anti-afk"].sneak) {
+        bot.setControlState("sneak", true);
+      }
+    }
+  });
+
+  // bot.on("chat", function (username, message) {
+  //   if (config.utils["chat-log"]) {
+  //     console.log(`[แชท] <${username}> ${message}`);
+  //   }
+  // });
+
+  bot.on("goal_reached", function () {
+    console.log(
+      `\x1b[32m[บันทึก] Bot arrived to target location. ${bot.entity.position}\x1b[0m`
+    );
+  });
+
+  bot.on("death", function () {
+    console.log(
+      `\x1b[33m[บันทึก] Bot has been died and was respawned ${bot.entity.position}`,
+      "\x1b[0m"
+    );
+  });
+
+  if (config.utils["auto-reconnect"]) {
+    bot.on("end", function () {
+        
+        // console.log("\nกำลังจะเชื่อมต่อเซิร์ฟเวอร์ในอีก");
+      var rdelay = config.utils["delay"];
+      console.log("กำลังจะเชื่อมต่อเซิร์ฟเวอร์ในอีก " + rdelay + " วินาที");
+      setTimeout(function () {
+        createBot();
+      }, rdelay * 1000);
+      // status(config.server.ip, config.server.port, (response) => {
+      //   if ((response.status == true)) {
+      //     console.log("เซิร์ฟเวอร์ออนไลน์");
+      //     createBot();
+      //   } else {
+      //     console.log("\x1b[31m",`เซิร์ฟเวอร์ออฟไลน์`,"\x1b[0m");
+      //     console.log("กำลังจะเชื่อมต่อเซิร์ฟเวอร์ในอีก " + rdelay + " วินาที");
+      //     // countDown(rdelay);
+          
+      //   }
+      // });
+      
+    })
+  }
+
+  bot.on("kicked", (reason) =>
+    console.log(
+      "\x1b[33m",
+      `[บันทึก] บอทโดนเตะออกจากเซิร์ฟเวอร์ด้วยเหตุผล : \n${reason}`,
+      "\x1b[0m"
+    )
+  );
+  bot.on("error", (err) =>
+    console.log(`\x1b[31m[เชื่อมต่อล้มเหลว] ${err.message}\n`, "\x1b[0m")
+  );
+}
+
+createBot();
