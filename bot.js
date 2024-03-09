94647b59a234e437c190335c01f0faf972dcc968
mhmtasd <162480356+mhmtasd@users.noreply.github.com>
2024-03-09 14:21:30 +0300

    bot.js

--- bot.js
+++ bot.js
@@ -8,83 +8,91 @@ const config = require("./settings.json");
 function countDown(number) {
   console.log("Is about to connect the server again in " + number + " seconds");
   if (number > 0) {
-      setTimeout(function(){
-        countDown(number - 10);
-      }, 10000)
+    setTimeout(function () {
+      countDown(number - 10);
+    }, 10000);
   }
 }
 
 function countDownMinutes(numberM) {
-  console.log("The bots will disconnect from the server in " + numberM / 60 / 60 + " hours");
-    if (numberM > 0) {
-      setTimeout(function(){
-        countDownMinutes(numberM - 600);
-      }, 600000)
+  console.log(
+    "The bots will disconnect from the server in " +
+      numberM / 60 / 60 +
+      " hours"
+  );
+  if (numberM > 0) {
+    setTimeout(function () {
+      countDownMinutes(numberM - 600);
+    }, 600000);
   }
 }
 
 function disconnectBot() {
-  var timeout = config.utils["timeout"];
-      countDownMinutes(timeout);
-      setTimeout(function () {
-        console.log("The bots have been disconnected");
-        // bot.end();
-        process.exit();
-      }, timeout * 1000);
+  const timeout = config.utils["timeout"];
+  countDownMinutes(timeout);
+  setTimeout(function () {
+    console.log("The bots have been disconnected");
+    process.exit();
+  }, timeout * 1000);
 }
 
 function createBot() {
-  const bot = mineflayer.createBot({
+  // Initialize bot settings as an empty object
+  const bot = {
+    settings: {},
     username: config["bot-account"]["username"],
     password: config["bot-account"]["password"],
     auth: config["bot-account"]["type"],
     host: config.server.ip,
     port: config.server.port,
     version: config.server.version,
-  });
+  };
 
   bot.loadPlugin(pathfinder);
   const mcData = require("minecraft-data")(bot.version);
   const defaultMove = new Movements(bot, mcData);
+
+  // Set colorsEnabled after bot settings initialization
   bot.settings.colorsEnabled = false;
 
   bot.once("spawn", function () {
-    var name = config["bot-account"]["username"];
+    const name = config["bot-account"]["username"];
     console.log("\x1b[33m[BotLog] " + name + " joined to the server", "\x1b[0m");
 
     if (config.utils["auto-auth"].enabled) {
       console.log("[INFO] Started auto-auth module");
 
-      var password = config.utils["auto-auth"].password;
+      const password = config.utils["auto-auth"].password;
       setTimeout(function () {
         bot.chat(`/register ${password} ${password}`);
         bot.chat(`/login ${password}`);
       }, 500);
 
-      console.log(`[Auth] Authentification commands executed.`)
+      console.log(`[Auth] Authentification commands executed.`);
+      // Optional: Memory usage logging (commented out)
       // const used = process.memoryUsage().heapUsed / 1024 / 1024;
       // console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB\n`);
 
-      //if want to use this function
+      // Call disconnectBot if auto-auth is enabled
       disconnectBot();
-
     }
-    
-    
+
     if (config.utils["chat-messages"].enabled) {
       console.log("[INFO] Started chat-messages module");
-      var messages = config.utils["chat-messages"]["messages"];
+      const messages = config.utils["chat-messages"]["messages"];
 
       if (config.utils["chat-messages"].repeat) {
-        var delay = config.utils["chat-messages"]["repeat-delay"];
+        const delay = config.utils["chat-messages"]["repeat-delay"];
         let i = 0;
 
-        let msg_timer = setInterval(() => {
+        const msg_timer = setInterval(() => {
           bot.chat(`${messages[i]}`);
 
-          if (i + 1 == messages.length) {
+          if (i + 1 === messages.length) {
             i = 0;
-          } else i++;
+          } else {
+            i++;
+          }
         }, delay * 1000);
       } else {
         messages.forEach(function (msg) {
@@ -100,48 +108,4 @@ function createBot() {
         `\x1b[32m[BotLog] Starting moving to target location (${pos.x}, ${pos.y}, ${pos.z})\x1b[0m`
       );
       bot.pathfinder.setMovements(defaultMove);
-      bot.pathfinder.setGoal(new GoalBlock(pos.x, pos.y, pos.z));
-    }
-
-    if (config.utils["anti-afk"].enabled) {
-      bot.setControlState("jump", true);
-      if (config.utils["anti-afk"].sneak) {
-        bot.setControlState("sneak", true);
-      }
-    }
-  });
-
-  bot.on("chat", function (username, message) {
-    if (config.utils["chat-log"]) {
-      console.log(`[ChatLog] <${username}> ${message}`);
-    }
-  });
-
-  bot.on("goal_reached", function () {
-    console.log(
-      `\x1b[32m[BotLog] Bot arrived to target location. ${bot.entity.position}\x1b[0m`
-    );
-  });
-
-  bot.on("death", function () {
-    console.log(
-      `\x1b[33m[BotLog] Bot has been died and was respawned ${bot.entity.position}`,
-      "\x1b[0m"
-    );
-  });
-
-  if (config.utils["auto-reconnect"]) {
-    bot.on("end", function () {
-      var rdelay = config.utils["delay"];
-      countDown(rdelay);
-      setTimeout(function () {
-        console.log("Connect the server");
-        createBot();
-      }, rdelay * 1000);
-    })
-  } else 
-
-  bot.on('kicked', (reason) => console.log('\x1b[33m',`[BotLog] Bot was kicked from the server. Reason: \n${reason}`, '\x1b[0m'))
-  bot.on('error', err => console.log(`\x1b[31m[ERROR] ${err.message}`, '\x1b[0m'))
-}
-createBot();
+      bot.pathfinder.setGoal(new GoalBlock(pos.x
